b4f3b4ca9d24f717741cfe3f9aaef70b747266e2
dinozin <dinozindev@gmail.com>
2024-08-05 19:28:38 -0300

    nano course .NET

---
+++ FiapHelloWorld/FiapHelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria uma nova instancia da classe HelloModel
+            Models.HelloModel model = new Models.HelloModel();
+            // acessa a variável Mensagem da classe HelloModel
+            Console.WriteLine(model.Mensagem);
+            // trecho para manter a janela aberta
+            Console.Read();
+        }
+    }
+}
